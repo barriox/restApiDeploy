30b13c05da94074af7ad30451ee066823e6d496e
Barriox <mi.ba.la@hotmail.com>
2024-04-02 21:22:46 +0200

    simplified app using mvc

---
+++ models/movie.js
@@ -0,0 +1,41 @@
+import { readJSON } from "../utils/utils";
+import { randomUUID } from "node:crypto";
+const movies = readJSON("../movies.json");
+export class MovieModel {
+  static getAll = async ({ genre }) => {
+    if (genre) {
+      return movies.filter((movie) => movie.genre.includes(genre));
+    }
+    return movies;
+  };
+  static async getById({ id }) {
+    const movie = movies.find((movie) => movie.id == id);
+    return movie;
+  }
+  static async create(input) {
+    const newMovie = {
+      id: randomUUID(),
+      ...input,
+    };
+    movies.push(newMovie);
+    return newMovie;
+  }
+  static async delete({ id }) {
+    const deletingIndex = movies.findIndex((movie) => movie.id === id);
+    if (deletingIndex === -1) {
+      return false;
+    }
+    movies.splice(deletingIndex, 1);
+    return true;
+  }
+  static async update({ id, input }) {
+    const movieIndex = movies.findIndex((movie) => movie.id == id);
+
+    const updatedMovie = {
+      ...movies[movieIndex],
+      ...input,
+    };
+    movies[movieIndex] = updatedMovie;
+    return updatedMovie;
+  }
+}
